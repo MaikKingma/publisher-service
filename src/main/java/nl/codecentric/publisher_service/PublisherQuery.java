2b117c9fc86cf74952a30b1aebd4acfcabeb9c96
Maik Kingma <maik.kingma@codecentric.nl>
2022-07-29 16:38:25 +0200

    added query by ID

--- src/main/java/nl/codecentric/publisher_service/PublisherQuery.java
+++ src/main/java/nl/codecentric/publisher_service/PublisherQuery.java
@@ -1,10 +1,12 @@
 package nl.codecentric.publisher_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Maik Kingma
@@ -23,5 +25,10 @@ public class PublisherQuery {
     @GetMapping
     public List<Publisher> findAll() {
         return publisherService.getAllPublishers();
+    }
+
+    @GetMapping("/{id}")
+    public Publisher findAll(@PathVariable("id") UUID uuid) throws PublisherService.PublisherNotFoundException {
+        return publisherService.getPublisherById(uuid);
     }
 }
